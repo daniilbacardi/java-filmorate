b2ab4cd3279164b8d531f12e1a0ec99830a09b6d
Daniil Kuznetsov <daniilbacardi@yandex.ru>
2023-06-03 18:56:13 +0300

    вернул случайно удаленную аннотацию

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @Valid
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
+    @JsonIgnore
     int id;
     @NotBlank
     @Email
